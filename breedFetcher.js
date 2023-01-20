8f0dd364bae73305b9b3856a6ce3cd0870df7b01
Zaima Atoshi <zaimaz.atoshi@gmail.com>
2023-01-19 23:19:35 -0500

    added test using mocha

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,10 +1,29 @@
 const request = require('request');
-const search = process.argv[2];
-const site = "https://api.thecatapi.com/v1/breeds/search?q="+search;
+//const search = process.argv[2];
 
-request(site, (error, response, body) => {
-  console.log('error:', error); // Print the error if one occurred
-  console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
-  console.log(typeof body);
-  
-});
+const fetchBreedDescription = function (breedName, callback) {
+  const site = "https://api.thecatapi.com/v1/breeds/search?q=";
+  request(site + breedName, (error, response, body) => {
+    try {
+      const data = JSON.parse(body)[0];
+      const keysNames = Object.keys(data);
+      callback(null, data["description"]);
+    } catch (error) {
+      callback(error, null);
+    }
+
+
+
+    // if (error === null) { //if no error is found
+    //   callback(null,data["description"]); 
+    // } else { // if error is found
+    //   callback(error,null);
+    // }
+
+  });
+}
+
+module.exports = { fetchBreedDescription }
+
+
+
